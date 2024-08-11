41df8f66904f9451f92d702230b0c86931418f6f
Shashank <113260615+shashankaz@users.noreply.github.com>
2024-08-11 14:19:04 +0530

    added analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -41,6 +42,7 @@ const App = () => {
           <Footer />
         </Router>
       </Suspense>
+      <Analytics />
     </div>
   );
 };
